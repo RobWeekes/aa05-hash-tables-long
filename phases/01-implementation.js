88e4d0a8b80e2a21d05571791b6aded41416f99a
Robert Weekes <weekes.robert@gmail.com>
2024-06-27 16:41:56 -0700

    phase1 hash table passing 5/10

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -8,45 +8,64 @@ class KeyValuePair {
 
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
-  constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here 
-  }
-
-  hash(key) {
-    let hashValue = 0;
-
-    for (let i = 0; i < key.length; i++) {
-      hashValue += key.charCodeAt(i);
+    constructor(numBuckets = 8) {
+        this.capacity = numBuckets;
+        this.data = new Array(numBuckets).fill(null);
+        this.count = 0;
     }
 
-    return hashValue;
-  }
+    hash(key) {
+      let hashValue = 0;
 
-  hashMod(key) {
-    // Get index after hashing
-    return this.hash(key) % this.capacity;
-  }
+      for (let i = 0; i < key.length; i++) {
+          hashValue += key.charCodeAt(i);
+      }
+
+      return hashValue;
+    }
+
+    hashMod(key) {
+        // Get index after hashing
+        return this.hash(key) % this.capacity;
+    }
+
+    insert(key, value) {
+        const index = this.hashMod(key);
+        // console.log(index);
+        let currentPair = this.data[index];
+
+        // check if there's already a value at the hashed index
+        while(currentPair && currentPair.key !== key) {   // check for KEY matches, stop if it finds one, or it reaches the end
+            currentPair = currentPair.next;   // steps thru until it matches a key, or reaches the end/tail
+        }
+        if(currentPair) {   // if we have a matching key, line 42 executes. If we reach the tail, it does not execute because 'currentPair' will be set to 'null', (currentPair.next of the tail line 39)
+            currentPair.value = value;  // OVERWRITE -- if not at the tail, change the value of the node that matched the input 'key'
+        } else {    // if we found no matches, 'else' block happens bcuz there were no key matches along the way \/
+            const newPair = new KeyValuePair(key, value);   // if there were no key matches in the linked list, assign the newPair as the head node \/
+            // assign newPair as the head
+            if(!this.data[index]) {   // NO COLLISION condition, empty bucket -- insert new pair at the empty index
+                this.data[index] = newPair;   // EMPTY BUCKET -> insert the new pair as is
+            } else {
+                newPair.next = this.data[index]; // ->  ->  ->  COLLISION condition -- insert new pair and assign new pair's 'next' attribute to the old pair 'this.data[index]'
+                this.data[index] = newPair;       
+            }   // 'this.data[index]' is the bucket. Set the new pair as the bucket's value, and point to the previous pair if one already existed, using 'newPair.next = this.data[index];'
+            this.count++;
+        }
+        }
+
+    read(key) {
+
+    }
 
 
-  insert(key, value) {
-    // Your code here 
-  }
+    resize() {
+
+    }
 
 
-  read(key) {
-    // Your code here 
-  }
+    delete(key) {
 
-
-  resize() {
-    // Your code here 
-  }
-
-
-  delete(key) {
-    // Your code here 
-  }
+    }
 }
 
 

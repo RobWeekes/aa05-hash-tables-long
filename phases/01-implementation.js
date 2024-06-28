0d89d6636ee2cb5639331b001120b5c8a0690881
Khalid <mugen89k@gmail.com>
2024-06-27 20:28:47 -0400

    not done

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -6,78 +6,77 @@ class KeyValuePair {
   }
 }
 
-class HashTable { // get O(1), set O(1), deleteKey O(1)
+class HashTable {
+  // get O(1), set O(1), deleteKey O(1)
 
-    constructor(numBuckets = 8) {
-        this.capacity = numBuckets;
-        this.data = new Array(numBuckets).fill(null);
-        this.count = 0;
+  constructor(numBuckets = 8) {
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null);
+    this.count = 0;
+  }
+
+  hash(key) {
+    let hashValue = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hashValue += key.charCodeAt(i);
     }
 
-    hash(key) {
-      let hashValue = 0;
+    return hashValue;
+  }
 
-      for (let i = 0; i < key.length; i++) {
-          hashValue += key.charCodeAt(i);
-      }
+  hashMod(key) {
+    // Get index after hashing
+    return this.hash(key) % this.capacity;
+  }
 
-      return hashValue;
+  insert(key, value) {
+    const index = this.hashMod(key);
+    // console.log(index);
+    let currentPair = this.data[index];
+
+    // check if there's already a value at the hashed index
+    while (currentPair && currentPair.key !== key) {
+      // check for KEY matches, stop if it finds one, or it reaches the end
+      currentPair = currentPair.next; // steps thru until it matches a key, or reaches the end/tail
     }
-
-    hashMod(key) {
-        // Get index after hashing
-        return this.hash(key) % this.capacity;
+    if (currentPair) {
+      // if we have a matching key, line 42 executes. If we reach the tail, it does not execute because 'currentPair' will be set to 'null', (currentPair.next of the tail line 39)
+      currentPair.value = value; // OVERWRITE -- if not at the tail, change the value of the node that matched the input 'key'
+    } else {
+      // if we found no matches, 'else' block happens bcuz there were no key matches along the way \/
+      const newPair = new KeyValuePair(key, value); // if there were no key matches in the linked list, assign the newPair as the head node \/
+      // assign newPair as the head
+      if (!this.data[index]) {
+        // NO COLLISION condition, empty bucket -- insert new pair at the empty index
+        this.data[index] = newPair; // EMPTY BUCKET -> insert the new pair as is
+      } else {
+        newPair.next = this.data[index]; // ->  ->  ->  COLLISION condition -- insert new pair and assign new pair's 'next' attribute to the old pair 'this.data[index]'
+        this.data[index] = newPair;
+      } // 'this.data[index]' is the bucket. Set the new pair as the bucket's value, and point to the previous pair if one already existed, using 'newPair.next = this.data[index];'
+      this.count++;
     }
+  }
 
-    insert(key, value) {
-        const index = this.hashMod(key);
-        // console.log(index);
-        let currentPair = this.data[index];
+  read(key) {
+    const index = this.hashMod(key);
+    let currentPair = this.data[index];
 
-        // check if there's already a value at the hashed index
-        while(currentPair && currentPair.key !== key) {   // check for KEY matches, stop if it finds one, or it reaches the end
-            currentPair = currentPair.next;   // steps thru until it matches a key, or reaches the end/tail
-        }
-        if(currentPair) {   // if we have a matching key, line 42 executes. If we reach the tail, it does not execute because 'currentPair' will be set to 'null', (currentPair.next of the tail line 39)
-            currentPair.value = value;  // OVERWRITE -- if not at the tail, change the value of the node that matched the input 'key'
-        } else {    // if we found no matches, 'else' block happens bcuz there were no key matches along the way \/
-            const newPair = new KeyValuePair(key, value);   // if there were no key matches in the linked list, assign the newPair as the head node \/
-            // assign newPair as the head
-            if(!this.data[index]) {   // NO COLLISION condition, empty bucket -- insert new pair at the empty index
-                this.data[index] = newPair;   // EMPTY BUCKET -> insert the new pair as is
-            } else {
-                newPair.next = this.data[index]; // ->  ->  ->  COLLISION condition -- insert new pair and assign new pair's 'next' attribute to the old pair 'this.data[index]'
-                this.data[index] = newPair;       
-            }   // 'this.data[index]' is the bucket. Set the new pair as the bucket's value, and point to the previous pair if one already existed, using 'newPair.next = this.data[index];'
-            this.count++;
-        }
-        }
-
-    read(key) {
-        const index = this.hashMod(key);
-        let currentPair = this.data[index];
-        
-        while(currentPair && currentPair.key !== key) {
-            currentPair = currentPair.next;
-        }
-        if(this.data[index].key === key) {
-            return this.data[index].value;
-        } 
-        return undefined;
+    while (currentPair && currentPair.key !== key) {
+      currentPair = currentPair.next;
     }
-
-
-    resize() {
-
+    if (this.data[index].key === key) {
+      return this.data[index].value;
     }
+    return undefined;
+  }
 
+  resize() {}
 
-    delete(key) {
-
-    }
+  delete(key) {}
 }
 
-// local testing 
+// local testing
 
 // let hashTable = new HashTable(4);
 
@@ -97,7 +96,6 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 // console.log(hashTable.data[3].value) // "value2"
 // console.log(hashTable.data[0].value) // "value3"
 
-
 // hashTable = new HashTable(2);
 
 // console.log(hashTable.capacity) // (2);
@@ -111,7 +109,6 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 // console.log(hashTable.data[1].next.value) // ("value2")
 // console.log(hashTable.data[1].value) // ("value4")
 
-
 // hashTable = new HashTable(4);
 
 // hashTable.insert("key1", "value1")
@@ -122,35 +119,33 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 // console.log(hashTable.read("key2")) // "value2"
 // console.log(hashTable.read("key3")) // "value3"
 
-// console.log(hashTable.read("key5")) // undefined
+// // console.log(hashTable.read("key5")) // undefined
 
+// hashTable = new HashTable(2);
 
-hashTable = new HashTable(2);
+// console.log(hashTable.capacity); // 2
 
-console.log(hashTable.capacity) // 2
+// console.log(hashTable.hashMod("key1"));
+// console.log(hashTable.hashMod("key2"));
+// console.log(hashTable.hashMod("key3"));
+// console.log(hashTable.hashMod("key5"));
+// console.log(hashTable.hashMod("key9"));
+// console.log(hashTable.hashMod("key10"));
 
-console.log(hashTable.hashMod("key1"));
-console.log(hashTable.hashMod("key2"));
-console.log(hashTable.hashMod("key3"));
-console.log(hashTable.hashMod("key5"));
-console.log(hashTable.hashMod("key9"));
-console.log(hashTable.hashMod("key10"));
+// hashTable.insert("key1", "value1");
+// hashTable.insert("key2", "value2");
+// hashTable.insert("key3", "value3");
+// hashTable.insert("key5", "value5");
+// hashTable.insert("key9", "value9");
+// hashTable.insert("key10", "value10");
 
-hashTable.insert("key1", "value1")
-hashTable.insert("key2", "value2")
-hashTable.insert("key3", "value3")
-hashTable.insert("key5", "value5")
-hashTable.insert("key9", "value9")
-hashTable.insert("key10", "value10")
+// console.log(hashTable.read("key1")); // "value1";
+// console.log(hashTable.read("key2")); // "value2";
+// console.log(hashTable.read("key3")); // "value3";
+// console.log(hashTable.read("key5")); // "value5";
+// console.log(hashTable.read("key9")); // "value9";
+// console.log(hashTable.read("key10")); // "value10";
 
-console.log(hashTable.read("key1")) // "value1";
-console.log(hashTable.read("key2")) // "value2";
-console.log(hashTable.read("key3")) // "value3";
-console.log(hashTable.read("key5")) // "value5";
-console.log(hashTable.read("key9")) // "value9";
-console.log(hashTable.read("key10")) // "value10";
-
-console.log(hashTable.read("key20")) // undefined
-
+// console.log(hashTable.read("key20")); // undefined
 
 module.exports = HashTable;
